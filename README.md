5e292c90589c8c57f4bbf050fa751b2f4daa1c23
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-08-02 11:25:02 -0500

    update README with usage example and enhance encryption logic in lockdown.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LockDown
 Lockdown is an CLI encryption/decryption/hashing tool.
+
+node lockdown.js -f sample_input.txt --key "MySecretKey2025" --iv "MyInitVector" --hash sha512 --encrypt aes-256-cbc -o encrypted_output.txt
--- lockdown.js
+++ lockdown.js
@@ -2,11 +2,14 @@
 const minimist = require('minimist')
 const fs = require('fs')
 const colors = require('colors');
+const crypto = require('crypto')
 
 // set all basic stuff
 var argv;
 var content;
 var mute;
+var key = 'example' // default encryption key
+var iv = 'example' // default initialization vector key
 var output = 0
 
 // get arguments
@@ -33,6 +36,51 @@ if (argv['f']){
         console.error(`${colors.red(`Error while trying to fetch file (-f):`)} ${err}`)
     }
 }
+
+if (argv['key']){ // make sure you pass data or else --key becomes useless
+    let _key = argv['key']
+    if (typeof _key !== 'string'){
+        _key =  key
+    }
+    key = _key
+}
+if (argv['iv']){ // make sure you pass data or else --key becomes useless
+    let _iv = argv['iv']
+    if (typeof _iv !== 'string'){
+        _iv =  iv
+    }
+    iv = _iv
+}
+if (argv['hash']){
+    let method = argv['hash']
+    if (typeof method !== 'string'){
+        method =  'sha256'
+    }
+    try{
+        const hash = crypto.createHash(method)
+        hash.update(content)
+        content = hash.digest('hex')
+    } catch (err){
+        console.error(`${colors.red(`Error while trying to hash the value (--hash):`)} ${err}`)
+    }
+}
+if (argv['encrypt']){
+    let algorithm = argv['encrypt']
+    if (typeof algorithm !== 'string'){
+        algorithm =  'aes-256-cbc'
+    }
+    try{
+        let keyBuffer = crypto.createHash('sha256').update(key).digest()
+        let ivBuffer = crypto.createHash('md5').update(iv).digest()
+        
+        let cipher = crypto.createCipheriv(algorithm, keyBuffer, ivBuffer)
+        content = cipher.update(content,'utf8','hex')
+        content += cipher.final('hex')
+    } catch (err){
+        console.error(`${colors.red(`Error while trying to encrypt the value (--encrypt):`)} ${err}`)
+    }
+}
+
 if (argv['o']){
     try{
         let file = argv['o']
