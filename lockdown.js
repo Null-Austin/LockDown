985ffef46f565246869d8b93d3da2d6fd56c67fa
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-08-02 10:45:33 -0500

    basic options

--- lockdown.js
+++ lockdown.js
@@ -1,10 +1,37 @@
 // Libaries
 const minimist = require('minimist')
+const fs = require('fs')
+const colors = require('colors');
 
 // set all basic stuff
 var argv;
 var content;
+var output = 0
 
 // get arguments
 argv = minimist(process.argv.slice(2))
-console.log(argv)
+
+// custom functions
+function log(...data){
+    console.log(...data)
+}
+
+// checks
+if (argv['f']){
+    try{
+        let file = argv['f']
+        content = fs.readFileSync(file,'utf8')
+        log(content)
+    } catch (err){
+        console.error(`${colors.red(`Error while trying to fetch file (-f):`)} ${err}`)
+    }
+}
+if (argv['o']){
+    try{
+        let file = argv['o']
+        fs.writeFileSync(file,content)
+        log(colors.green(`Output saved to: ${colors.underline(file)}`))
+    } catch (err){
+        console.error(`${colors.red(`Error while trying to write to file (-o):`)} ${err}`)
+    }
+}
