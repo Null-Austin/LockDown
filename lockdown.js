453657a60b043e4ccfe81662bc33c60ed56f2d3c
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-08-02 10:34:48 -0500

    updated

--- lockdown.js
+++ lockdown.js
@@ -1,2 +1,10 @@
-var argv = require('minimist')(process.argv.slice(2));
-console.dir(argv);
+// Libaries
+const minimist = require('minimist')
+
+// set all basic stuff
+var argv;
+var content;
+
+// get arguments
+argv = minimist(process.argv.slice(2))
+console.log(argv)
